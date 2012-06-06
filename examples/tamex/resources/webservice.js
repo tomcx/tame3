b2ddccb77cebaca67584aa75cfb9d5420829582c
Thomas Schmidt <tom@TomsDev.werkstatt.xx>
2012-06-06 08:26:24 +0200

    Test using relative path in SymFile-URL.

--- examples/tamex/resources/webservice.js
+++ examples/tamex/resources/webservice.js
@@ -9,5 +9,5 @@ var Plc =  TAME.WebServiceClient.createClient({
     amsPort: '801',     //default
     //dataAlign4: true,  //default, set it to "true" if you have an ARM based PLC device (i.e. CX90xx)
     language: 'ge',      //default, set it to "en" for english names of days and months
-    symFileUrl: 'http://192.168.1.2/tame3/resources/demo.tpy'
+    symFileUrl: '../resources/demo.tpy'
 });
